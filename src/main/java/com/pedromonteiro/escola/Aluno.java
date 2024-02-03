36a80ab26c94c972cfbc068de4864b3cef949417
Pedro Monteiro <pedromont436@gmail.com>
2024-02-02 22:16:28 -0300

    criando classe indicacao

--- src/main/java/com/pedromonteiro/escola/Aluno.java
+++ src/main/java/com/pedromonteiro/escola/Aluno.java
@@ -1,5 +1,8 @@
 package com.pedromonteiro.escola;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aluno {
 
     // Entity
@@ -8,5 +11,19 @@ public class Aluno {
     private String nome;
     private Email email;
     
+    private List<Telefone> telefones = new ArrayList<>();
+
+
+    public Aluno(CPF cpf, String nome, Email email) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.email = email;
+    }
+
+
+
+    public void adicionarTelefone(String ddd, String numero) {
+        this.telefones.add(new Telefone(ddd, numero));
+    }
     
 }
---
+++ src/main/java/com/pedromonteiro/escola/Indicacao.java
@@ -0,0 +1,33 @@
+package com.pedromonteiro.escola;
+
+import java.time.LocalDateTime;
+
+public class Indicacao {
+
+    private Aluno indicante;
+    private Aluno indicado;
+    private LocalDateTime dataIndicacao;
+
+
+    public Indicacao(Aluno indicante, Aluno indicado) {
+        this.indicante = indicante;
+        this.indicado = indicado;
+        this.dataIndicacao = LocalDateTime.now();
+    }
+
+
+    public Aluno getIndicante() {
+        return indicante;
+    }
+
+
+    public Aluno getIndicado() {
+        return indicado;
+    }
+
+
+    public LocalDateTime getDataIndicacao() {
+        return dataIndicacao;
+    }
+    
+}
