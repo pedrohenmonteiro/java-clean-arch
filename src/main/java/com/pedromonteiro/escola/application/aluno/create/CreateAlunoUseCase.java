3c7dbd601a13943614b7492580cdb50229abd47d
Pedro Monteiro <pedromont436@gmail.com>
2024-03-06 00:06:36 -0300

    implementando use case de criar aluno

---
+++ src/main/java/com/pedromonteiro/escola/application/aluno/create/CreateAlunoUseCase.java
@@ -0,0 +1,7 @@
+package com.pedromonteiro.escola.application.aluno.create;
+
+import com.pedromonteiro.escola.application.UseCase;
+
+public abstract class CreateAlunoUseCase extends UseCase<CreateAlunoCommand, CreateAlunoOutput> {
+
+}
---
+++ src/main/java/com/pedromonteiro/escola/application/aluno/create/DefaultCreateAlunoUseCase.java
@@ -0,0 +1,24 @@
+package com.pedromonteiro.escola.application.aluno.create;
+
+import com.pedromonteiro.escola.domain.aluno.Aluno;
+import com.pedromonteiro.escola.domain.aluno.AlunoGateway;
+
+public class DefaultCreateAlunoUseCase extends CreateAlunoUseCase{
+    
+    private AlunoGateway alunoGateway;
+
+    @Override
+    public CreateAlunoOutput execute(CreateAlunoCommand umCommand) {
+
+        final var umAluno = Aluno.newAluno(
+            umCommand.cpf(),
+            umCommand.nome(),
+            umCommand.email()
+        );
+
+        return CreateAlunoOutput.from(this.alunoGateway.create(umAluno));
+
+    }
+
+
+}
