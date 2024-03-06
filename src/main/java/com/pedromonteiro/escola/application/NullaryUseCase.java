9feafd13c8542d41fe502c07e05d7541e2f1e241
Pedro Monteiro <pedromont436@gmail.com>
2024-03-05 23:34:04 -0300

    criando as bases para os casos de uso

---
+++ src/main/java/com/pedromonteiro/escola/application/NullaryUseCase.java
@@ -0,0 +1,6 @@
+package com.pedromonteiro.escola.application;
+
+public abstract class NullaryUseCase<OUT> {
+    
+    public abstract OUT execute();
+}
---
+++ src/main/java/com/pedromonteiro/escola/application/UnitUseCase.java
@@ -0,0 +1,6 @@
+package com.pedromonteiro.escola.application;
+
+public abstract class UnitUseCase<IN> {
+    
+    public abstract void execute(IN anIn);
+}
